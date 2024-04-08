b372ef783c0e3b1e7c0c5b19c6de707587743e8b
Rudra Kumar <rkumar_365@outlook.com>
2024-04-08 17:58:22 -0400

    Testing out drive.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys  # Allows us to use sys.path.append below
 import time # Allows us to insert delays in our script
 
@@ -15,33 +16,49 @@ rvr.wake()
 time.sleep(2)
 
 def main():
-    # Make sure RVR is awake and ready to receive commands
-    rvr.wake()
-    
-    # Wait for RVR to wake up
-    time.sleep(2)
-    
-    # Set all of RVR's LEDs to a named color
-    rvr.led_control.set_all_leds_color(color=Colors.yellow)
-    
-    # Wait a second so we get to see the color change
-    time.sleep(1)
-    
-    # Now try using RGB (Red Green Blue) values.  
-    # This allows us to pick any color in the RGB colorspace.
-    rvr.led_control.set_all_leds_rgb(red=255, green=0, blue=0)
+    """ This program has RVR drive with how to drive RVR using the drive control helper.
+    """
 
-# This tells the interpreter to run our main() function if it is running this script 
-# directly (and not importing it as a module from elsewhere)
-if __name__ == '__main__':
     try:
-        # Stuff to try doing (which might generate an exception)
-        main()
+        rvr.wake()
+
+        # Give RVR time to wake up
+        time.sleep(2)
+
+        rvr.drive_control.reset_heading()
+
+        rvr.drive_control.drive_forward_seconds(
+            speed=64,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=1
+        )
+
+        # Delay to allow RVR to drive
+        time.sleep(1)
+
+        rvr.drive_control.drive_backward_seconds(
+            speed=64,
+            heading=0,  # Valid heading values are 0-359
+            time_to_drive=1
+        )
+
+        # Delay to allow RVR to drive
+        time.sleep(1)
+
+        rvr.drive_control.turn_left_degrees(
+            heading=0,  # Valid heading values are 0-359
+            amount=90
+        )
+
+        # Delay to allow RVR to drive
+        time.sleep(1)
+
     except KeyboardInterrupt:
-        # What to do if there's a keyboard interrupt (ctrl+c) exception
-        # In this case, we're just going to print a message
         print('\nProgram terminated with keyboard interrupt.')
 
     finally:
-        # What to do before we exit the block
         rvr.close()
+
+
+if __name__ == '__main__':
+    main()
